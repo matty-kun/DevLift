9e3cc105ccfa3e8a7e15ce97eb465e6664c25f99
Jan Matthew Vargas <157191677+matty-kun@users.noreply.github.com>
2025-05-17 09:38:51 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-
-
-
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <main className="flex-grow"></main>
+        <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />}/>
             {/* Add mre routes as needed */}
