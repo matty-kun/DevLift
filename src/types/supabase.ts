5eee668c6db68c04ea273f34df5c273168fdbd26
Jan Matthew Vargas <157191677+matty-kun@users.noreply.github.com>
2025-05-30 12:13:13 +0800

    Create supabase.ts

---
+++ src/types/supabase.ts
@@ -0,0 +1,11 @@
+export interface Database {
+  public: {
+    Tables: {
+      [key: string]: {
+        Row: Record<string, unknown>;
+        Insert: Record<string, unknown>;
+        Update: Record<string, unknown>;
+      };
+    };
+  };
+}
